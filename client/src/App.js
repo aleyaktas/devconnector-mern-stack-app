b4cf32e31b76b81651299eb68e7dab1d6ec5e8e7
Aleyna Aktaş <aleynaaktas627@gmail.com>
2021-07-05 15:01:29 +0300

    Create profile component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 //Redux
@@ -35,6 +36,7 @@ const App = () => {
             <Route exact path="/register" component={Register}/>
             <Route exact path="/login" component={Login}/>
             <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+            <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
           </Switch>
         </section>
       </Fragment>
