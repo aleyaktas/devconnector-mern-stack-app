84dd090ade9e48e3766c23ce1ab0eeb6f0f87f34
Aleyna Aktaş <aleynaaktas627@gmail.com>
2021-07-12 16:32:42 +0300

    Prepare for deployment to heroku

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const morgan = require('morgan');
-const app = express();
-var cors = require('cors');
+const path = require('path')
 
 app.use(cors());
 
@@ -13,14 +11,21 @@ app.use(morgan("dev"))
 // Init Middleware
 app.use(express.json({ extended: false }))
 
-app.post('/', (req, res) => res.send('API running'))
-
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 
+// Serve static assets in productıon
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
 
-const PORT = 5000;
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
